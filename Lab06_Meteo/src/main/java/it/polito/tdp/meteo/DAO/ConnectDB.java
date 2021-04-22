6aed7104746f83db9c8c7d9c8d7439402d89adf3
simone <simone@Lenovo-PC.station>
2021-04-22 09:31:36 +0200

    implementazione pooling DB

--- Lab06_Meteo/src/main/java/it/polito/tdp/meteo/DAO/ConnectDB.java
+++ Lab06_Meteo/src/main/java/it/polito/tdp/meteo/DAO/ConnectDB.java
@@ -4,23 +4,45 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 public class ConnectDB {
 	
+	private static final String jdbcURL = "jdbc:mysql://localhost/meteo";
+	private static HikariDataSource ds;
 	// check user e password
-	static private final String jdbcUrl = "jdbc:mysql://localhost/meteo?user=root&password=simone";
+	//static private final String jdbcUrl = "jdbc:mysql://localhost/meteo?user=root&password=simone";
 	
 
 	public static Connection getConnection() {
 
+		if(ds == null) {
+			HikariConfig config = new HikariConfig();
+			config.setJdbcUrl(jdbcURL);
+			config.setUsername("root");
+			config.setPassword("simone");
+			
+			config.addDataSourceProperty("cachePrepStmts", true);
+			config.addDataSourceProperty("prepStmtChacheSize", 250);
+			config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+			
+			ds = new HikariDataSource(config);
+		}
 		try {
-				Connection connection = DriverManager.getConnection(jdbcUrl);
-				return connection;
+//				Connection connection = DriverManager.getConnection(jdbcUrl);
+//				return connection;
+			return ds.getConnection();
+			
 
 		} catch (SQLException e) {
 
-			e.printStackTrace();
-			throw new RuntimeException("Cannot get a connection " + jdbcUrl, e);
+//			e.printStackTrace();
+//			throw new RuntimeException("Cannot get a connection " + jdbcUrl, e);
+			System.err.println("Errore di connessione ad db");
+			throw new RuntimeException(e);
 		}
+		
 	}
 
 }
